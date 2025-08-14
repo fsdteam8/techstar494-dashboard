cd676f9c8a241afa2e3dd3d888d2bb61ad40d85b
Mamun-Hossain-dev <mh4559641@gmail.com>
2025-08-14 16:30:32 +0600

    integrate the api with send email button

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,21 +6,20 @@
 
 // export default DashboardLayout;
 
-
 // app/layout.tsx
 
-import type { ReactNode } from "react";
-import "@/app/globals.css";
-import ClientLayout from "@/components/overveiw-componet/AppSidebar";
-
-
+import type { ReactNode } from 'react'
+import '@/app/globals.css'
+import ClientLayout from '@/components/overveiw-componet/AppSidebar'
+import { Toaster } from 'sonner'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body  className="bg-[#F3F4F6]">
+      <body className="bg-[#F3F4F6]">
         <ClientLayout>{children}</ClientLayout>
+        <Toaster richColors position="top-right" />
       </body>
     </html>
-  );
+  )
 }
